dc9b1c838c9515ee5b5fc7f331b9edbddc2a44dd
BoBaHrA <vladimir.procopisin@gmail.com>
2024-12-23 17:51:27 +0100

    Доработка логина

--- users/urls.py
+++ users/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('approve-profile/<int:profile_id>/', views.approve_profile, name='approve_profile'),
     path('reject-profile/<int:profile_id>/', views.reject_profile, name='reject_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # ... другие маршруты
 ]
