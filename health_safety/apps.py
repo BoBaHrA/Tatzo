0ee9c38b469afcdf0b971a7f971f10f8eb38e70f
BoBaHrA <vladimir.procopisin@gmail.com>
2026-08-19 14:20:31 +0200

    Configure Health and Safety app

---
+++ health_safety/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class HealthSafetyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "health_safety"
+    verbose_name = "Tatzo Health & Safety"
+
+    def ready(self):
+        from . import signals  # noqa: F401
